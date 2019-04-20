fb0a0264fe82f00dd19fcab5a51ab070616dd9f2
lijing1024 <295119348@qq.com>
2019-04-20 19:21:55 +0800

    登录框引入

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,51 @@
 <template>
-<p>登录</p>
+  <div class="container">
+    <div class="login_box">
+      <img src="../assets/images/logo.png" alt="logo">
+      <el-form :model="form">
+        <el-form-item>
+            <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 <style>
-
+  .container{
+      position: relative;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(30deg,#fff, #659097);
+  }
+  .container .login_box{
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -60%);
+      width: 400px;
+      height: 250px;
+      padding: 0 15px;
+      background: linear-gradient(210deg,#fff, rgb(162, 201, 201));
+      box-shadow: 0 0 10px #fff;
+  }
+  .container .login_box img {
+      display: block;
+      margin:15px auto;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 导入elment-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 导入全局样式
+import './assets/css/global.css'
+Vue.use(ElementUI, {size: 'small'})
+
+// 设置为true,控制台的日志会更详细
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
