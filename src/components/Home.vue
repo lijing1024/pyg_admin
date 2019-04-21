b65c297c499bc304a9b4719f7a9c789dc8d4bf10
lijing1024 <295119348@qq.com>
2019-04-21 15:39:34 +0800

    首页侧边栏导航菜单绘制&数据渲染

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,68 @@
 <template>
   <el-container class="home_container">
-    <el-header class="home_header">Header</el-header>
+    <el-header class="home_header">
+      <el-button @click="toggleMenu()" icon="iconfont icon-menu" size="mini" circle></el-button>
+      <span class="title">品优购后台管理系统</span>
+      <el-button class="exit" type="danger" round>退出</el-button>
+    </el-header>
     <el-container>
-      <el-aside class="home_aside" width="200px">Aside</el-aside>
-      <el-main class="home_main">Main</el-main>
+      <el-aside class="home_aside" :width="iscollapse?'65px':'180px'">
+        <el-menu
+          unique-opened
+          @open="handleOpen"
+          @close="handleClose"
+          :collapse="iscollapse"
+          :collapse-transition="false"
+          style="border:none;margin-top:5px"
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu v-for="(val, key) in menus" :key="val.id" :index="key+''">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>{{val.authName}}</span>
+            </template>
+            <el-menu-item :index="key +'-'+ (index+1)" v-for="(item, index) in val.children" :key="item.id">{{item.authName}}</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="home_main">欢迎来到品优购后台管理系统</el-main>
     </el-container>
   </el-container>
 </template>
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data(){
+    return {
+      iscollapse: false,
+      menus: [],
+      children: []
+    }
+  },
+  methods: {
+    // 折叠or展开导航菜单
+    toggleMenu () {
+      this.collapse=!this.collapse
+    },
+    // 获取菜单数据
+    loadData () {
+      this.$axios.get('menus')
+        .then(res => {
+          this.menus = res.data.data
+          this.children = res.data.data.children
+        })
+    },
+    handleOpen (key) {
+      console.log(key)
+    },
+    handleClose (key) {
+      console.log(key)
+    }
+  },
+  mounted () {
+    this.loadData()
+  }
 }
 </script>
 <style>
@@ -18,11 +71,22 @@ export default {
   }
   .home_header{
     background-color: #373D41;
+    line-height: 60px;
+  }
+  .home_header .exit{
+    float: right;
+    margin-top: 15px;
+  }
+  .home_header .title{
+    color: #fff;
+    margin-left: 10px;
+    font-size: 18px;
   }
   .home_aside{
     background-color: #333744;
   }
   .home_main{
     background-color: #EAEDF1;
+    font-size: 24px;
   }
 </style>
--- src/http/index.js
+++ src/http/index.js
@@ -4,5 +4,20 @@ import axios from 'axios'
 // 配置baseURL
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
+// 拦截器
+axios.interceptors.request.use(config => {
+  // 将token信息追加到请求头中
+  config.headers.Authorization = sessionStorage.getItem('token')
+  return config
+})
+// 响应时判断token令牌是否失效
+axios.interceptors.response.use(res => {
+  if (res.data.meta.status === 401) {
+    location.href = '/login'
+  }
+  else {
+    return res
+  }    
+})
 // 导出axios
 export default axios
