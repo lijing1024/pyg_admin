4d1fc521816e1dabcb6552c427bf54dc4b0a4e2b
lijing1024 <295119348@qq.com>
2019-04-25 01:48:24 +0800

    权限列表渲染完毕

--- src/components/permisson/Rights.vue
+++ src/components/permisson/Rights.vue
@@ -6,6 +6,20 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <el-card>
+      <el-table :data="rightsList" height="400px" highlight-current-row style="width: 100%">
+        <el-table-column type="index" width="50"></el-table-column>
+        <el-table-column property="authName" label="权限名称"></el-table-column>
+        <el-table-column property="path" label="路径"></el-table-column>
+        <el-table-column label="权限等级">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级权限</el-tag>
+            <el-tag v-if="scope.row.level === '1'" type="success">二级权限</el-tag>
+            <el-tag v-if="scope.row.level === '2'" type="warning">三级权限</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 <script>
--- src/components/permisson/RightsMixin.js
+++ src/components/permisson/RightsMixin.js
@@ -1,3 +1,20 @@
 export default {
-  name: 'Rights'
+  name: 'Rights',
+  data () {
+    return {
+      rightsList: []
+    }
+  },
+  methods: {
+    // 获取权限数据列表
+    async loadData () {
+      const {data: {data, meta}} = await this.$axios.get('rights/list')
+      if (meta.status !== 200) return this.$message.error('获取权限列表数据失败')
+      this.rightsList = data
+    }
+
+  },
+  mounted () {
+    this.loadData()
+  }
 }
